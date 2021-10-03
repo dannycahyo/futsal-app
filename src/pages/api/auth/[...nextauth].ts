36d9e1290e525b238d854e6593dd59d8bdeab686
Danny Dwi Cahyono <dannydwicahyono@gmail.com>
2021-10-03 13:27:08 +0700

    making-auth-api

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
+
+export default NextAuth({
+    session: {
+        jwt: true
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET
+    },
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  pages: {
+      signIn: "/login",    
+  }
+})
